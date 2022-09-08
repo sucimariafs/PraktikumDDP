c5420f1f803b01cf44fed49c7b07b808130cfb26
Suci Maria Febrina Siregar <112611711+sucimariafs@users.noreply.github.com>
2022-09-08 15:00:37 +0700

    Add files via upload

---
+++ SuciMariaFebrinaSiregar_C.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int x , y;
+	
+	cin >> x >> y;
+	
+	cout << 10 + 8 << endl;
+	cout << 10 - 8 << endl;
+	cout << 10 * 8 << endl;
+	cout << 10 / 8 << endl;
+	cout << 10 % 8 << endl;
+	
+	return 0;
+		
+}
