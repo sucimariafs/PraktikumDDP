01a2b6e429aa5fe08cf0b7955fed719f64963bcd
Suci Maria Febrina Siregar <112611711+sucimariafs@users.noreply.github.com>
2022-09-25 09:24:59 +0700

    Add files via upload

---
+++ 2217051002_SuciMariaFebrinaSiregar_Posttest1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    int angka;
+    float aaaaa;
+    string huruf;
+    char z;
+
+    cin >> angka;
+    cin >> aaaaa;
+    cin >> huruf;
+    cin >> z;
+
+    cout << "integer : " << angka << endl;
+    cout << "float : " << aaaaa << endl;
+    cout << "string : " << huruf << endl;
+    cout << "char : " << z << endl;
+
+    return 0;
+
+}
