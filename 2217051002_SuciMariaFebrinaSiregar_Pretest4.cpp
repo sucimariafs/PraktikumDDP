5e784fe04be2a8dc64f3fe9b0d837c6f09be31d5
Suci Maria Febrina Siregar <112611711+sucimariafs@users.noreply.github.com>
2022-09-29 12:08:37 +0700

    Add files via upload

---
+++ 2217051002_SuciMariaFebrinaSiregar_Pretest4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	int berat;
+	int tinggi;
+	
+	cin >> berat;
+	cin >> tinggi;
+	
+	cout << berat << "gemuk" << endl;
+	cout << tinggi << "kurus" << endl;
+	
+	return 0;
+	
+}
