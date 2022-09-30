ef8a06ccaedfd755ab8ca1154ea7210e3ed0c44d
Suci Maria Febrina Siregar <112611711+sucimariafs@users.noreply.github.com>
2022-09-30 22:49:51 +0700

    Add files via upload

---
+++ 2217051002_SuciMariaFebrinaSiregar_Praktest4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	int x;
+	int n;
+	
+	cin >> x;
+	cin >> n;
+	
+	cout <<
+	  
+	for (int i = x;i < n; i+x){
+		cout << i << " ";
+	}
+	
+	return 0;
+	
+}
+
+
