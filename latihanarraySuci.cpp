92be635880f44916238de31a933dbb2fb932317b
Suci Maria Febrina Siregar <112611711+sucimariafs@users.noreply.github.com>
2022-10-13 23:00:50 +0700

    Add files via upload

---
+++ latihanarraySuci.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	// membuat array kosong
+	int nilai [5];
+	
+	// mengisi array
+	nilai [0] = 32;
+	nilai [1] = 42;
+	nilai [2] = 76;
+	nilai [3] = 31;
+	nilai [4] = 57;
+	
+	// mencetak isi array
+	cout << "Nilai ke-1: " << nilai [0] << endl;
+	cout << "Nilai ke-2: " << nilai [1] << endl;
+	cout << "Nilai ke-3: " << nilai [2] << endl;
+	cout << "Nilai ke-4: " << nilai [3] << endl;
+	cout << "Nilai ke-5: " << nilai [4] << endl;
+	
+	return 0;
+	
+}
